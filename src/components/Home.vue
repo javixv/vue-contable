f3b22da2082f2c36465268509d55a3e7967db880
javixv <javixv@local.com>
2023-06-23 23:00:23 -0600

    modal, action, formato moneda

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,27 +3,79 @@ import { ref } from 'vue'
 import Header from './Header.vue'
 import Layout from './Layout.vue'
 import Resume from "./Resume/Index.vue";
-import Movements from "./Movements.vue";
+import Movements from "./Movements/Index.vue";
+import Action from "./Action.vue";
+
 
 const amount = ref(null);
+const label = ref(null);
+const movements = ref([{
+                id: 0,
+                title: "Movimiento 1",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 1,
+                title: "Movimiento 2",
+                description: "Lorem ipsum dolor sit amet",
+                amount: -1000,
+            }, {
+                id: 2,
+                title: "Movimiento 3",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 3,
+                title: "Movimiento 4",
+                description: "Lorem ipsum dolor sit amet",
+                amount: -1000,
+            }, {
+                id: 4,
+                title: "Movimiento 5",
+                description: "Lorem ipsum dolor sit amet",
+                amount: -1000,
+            }, {
+                id: 6,
+                title: "Movimiento 7",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 7,
+                title: "Movimiento 8",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 8,
+                title: "Movimiento 9",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }])
 </script>
+
 <template>
     <Layout>
         <template #header>
             <Header></Header>
         </template>
         <template #resume>
-            <Resume :total-label="'Ahorro total'" :label="label" :total-amount="1000000" :amount="amount">
-            <template #graphic>
-                graphic
-            </template>
-            <template #action>
-                action
-            </template>
+            <Resume
+                :total-label="'Ahorro total'"
+                :label="label"
+                :total-amount="1000000"
+                :amount="amount"
+            >
+                <template #graphic>
+                    graphic
+                </template>
+                <template #action>
+                    <Action/>
+                </template>
             </Resume>
         </template>
         <template #movements>
-            <Movements />
+            <Movements
+                :movements="movements"
+            />
         </template>
     </Layout>
 </template>
